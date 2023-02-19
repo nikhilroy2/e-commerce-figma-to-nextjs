f5f9c65eb89645b528f9578acd4c3445a1f21a11
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2023-02-19 20:43:15 +0600

    update 19th feb

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,9 +14,58 @@ export default function Home() {
 }
 
 function EcommerceProject() {
-  return <div id="eCommerceProject">
-    <div className="header">
-      
+  return (
+    <div id="eCommerceProject">
+      <div id="header">
+        <h3 className="header_title">E-Commerce</h3>
+        <button className="header_box">
+          <Image
+            width="24"
+            height="24"
+            style={{ marginRight: "5px" }}
+            src="/assets/icons/Shopping_cart.svg"
+            alt="icon"
+          ></Image>
+          <span className="font_sm">3</span>
+        </button>
+      </div>
+      <EccomerceNavbar></EccomerceNavbar>
     </div>
-  </div>;
+  );
+}
+
+function EccomerceNavbar() {
+  const nav_list = [
+    "Produce",
+    "Prepared foods",
+    "Canned foods & Soups",
+    "Produce",
+    "Bakery",
+    "Diary & Eggs",
+    "Frozen",
+    "Meat & Seafood",
+    "Bakery",
+    "Diary & Eggs",
+    "Prepared foods",
+    "Canned foods & Soups",
+    "Produce",
+    "Bakery",
+    "Diary & Eggs",
+    "Frozen",
+    "Meat & Seafood",
+  ];
+
+  return (
+    <nav id="eCommerceNavbar">
+      <ul className="navbar_list">
+        {nav_list.map((v, i) => {
+          return (
+            <li key={i}>
+              <button className="btn_tab">{v}</button>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
 }
